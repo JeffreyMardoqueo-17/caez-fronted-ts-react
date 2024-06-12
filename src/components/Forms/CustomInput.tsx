8366d9897eedba8087d4685e93ab2324d4401bc4
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-06-12 08:31:26 -0600

    Cambio del componten input, para que cambie el borde si ya hay datos

--- src/components/Forms/CustomInput.tsx
+++ src/components/Forms/CustomInput.tsx
@@ -1,8 +1,14 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { Input } from '@material-tailwind/react';
 import { CustomInputProps } from '../../interfaces/CustomInputProps';
 
 export const CustomInput: FC<CustomInputProps> = ({ label, placeholder, value, onChange }) => {
+    const [hasValue, setHasValue] = useState(false);
+
+    useEffect(() => {
+        setHasValue(value !== '');
+    }, [value]);
+
     return (
         <div className="w-full">
             <label className="mb-2 font-medium text-darkTheme-background dark:text-darkTheme-gray">
@@ -13,10 +19,11 @@ export const CustomInput: FC<CustomInputProps> = ({ label, placeholder, value, o
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
-                className="text-lightTheme-gray dark:text-darkTheme-gray dark:border-darkTheme-background"
+                className={`text-lightTheme-gray dark:text-darkTheme-gray ${hasValue ? 'border-blue-500' : 'border-none'}`}
                 onPointerEnterCapture={() => { } }
                 onPointerLeaveCapture={() => { } } 
-                crossOrigin={undefined}            />
+                crossOrigin={undefined}            
+            />
         </div>
     );
 };

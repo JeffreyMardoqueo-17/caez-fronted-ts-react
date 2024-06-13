533d2c57abd2016a4aaa1348bf3d7cc8081dc289
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-06-12 21:09:54 -0600

    Aqui todo bien con las tarjetas

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import MainTemplate from './components/views/public/MainTemplate';
 import Account1 from './components/acout/acout';
 import { Ejemplo } from './components/views/public/modales/Ejemplo';
 import { Table } from './components/Table/Table';
+import { FaMoneyBill, FaUser } from "react-icons/fa";
+import { DashboardCard } from './components/DashboardCard/DashboardCard';
 
 const App: React.FC = () => {
     const { theme, toggleTheme } = useTheme();
@@ -21,6 +23,32 @@ const App: React.FC = () => {
                 <div className="bg-lightTheme-background dark:bg-darkTheme-background min-h-screen">
                     <Menu toggleTheme={toggleTheme} theme={theme} />
                     {/* <Login onLogin={handleLogin} /> */}
+                    <div className="min-h-screen dark:bg-darkTheme-background p-8 flex flex-wrap items-center justify-center">
+                        <DashboardCard
+                            title="Total Revenue"
+                            amount="$5,000"
+                            Icon={FaMoneyBill}
+                            iconColor="text-green-500"
+                        />
+                        <DashboardCard
+                            title="Total Users"
+                            amount="1,200"
+                            Icon={FaUser}
+                            iconColor="text-blue-500"
+                        />
+                        <DashboardCard
+                            title="Total Revenue"
+                            amount="$5,000"
+                            Icon={FaMoneyBill}
+                            iconColor="text-green-500"
+                        />
+                        <DashboardCard
+                            title="Total Users"
+                            amount="1,200"
+                            Icon={FaUser}
+                            iconColor="text-blue-500"
+                        />
+                    </div>
                     <Account1 />
                     <Ejemplo />
                     <Table />
---
+++ src/components/DashboardCard/DashboardCard.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { IconType } from "react-icons";
+
+interface CardProps {
+    title: string;
+    amount: string;
+    Icon: IconType;
+    iconColor?: string;  // Añadimos una propiedad opcional para el color del icono
+}
+
+export const DashboardCard: React.FC<CardProps> = ({ title, amount, Icon, iconColor }) => {
+    return (
+        <div className="bg-lightTheme-background dark:bg-darkTheme-background shadow-md rounded-lg p-6 flex items-center justify-between border-2 dark:border-darkTheme-formulario border-gray-400 mr-2 ml-2 w-full sm:w-auto">
+            <div className="flex flex-col">
+                <h2 className="text-lg font-semibold text-lightTheme-gray dark:text-darkTheme-gray mb-2">{title}</h2>
+                <p className="text-2xl font-bold text-lightTheme-gray dark:text-darkTheme-text">{amount}</p>
+            </div>
+            <Icon className={`w-16 h-16 ml-4 ${iconColor}`} />
+        </div>
+    );
+};

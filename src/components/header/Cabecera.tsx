e937c48807b6caa6495229be33c35c950b2c1a64
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-06-18 12:44:25 -0600

    Enmaquetacion y colores lista

--- src/components/header/Cabecera.tsx
+++ src/components/header/Cabecera.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Cabecera = () => {
     return (
-        <div className="flex items-center justify-between bg-red-800 text-white p-4">
+        <div className="flex items-center justify-between bg-lightTheme-background dark:bg-darkTheme-formulario border-b-2 border-b-lightTheme-opacidad text-white p-4">
             <div className="text-lg font-bold">GESTOR DE PAGOS CAEZ</div>
         </div>
     );

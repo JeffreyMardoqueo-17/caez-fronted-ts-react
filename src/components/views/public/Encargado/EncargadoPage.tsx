4e899f2fec7eb227afdda5f9d639b4f02a6cb80c
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-07-04 08:37:15 -0600

    Filtraciones de encargado listo ✨🎉

--- src/components/views/public/Encargado/EncargadoPage.tsx
+++ src/components/views/public/Encargado/EncargadoPage.tsx
@@ -32,41 +32,42 @@ const EncargadoPage = () => {
             console.error('Error al obtener los datos:', error);
         }
     };
+
     useEffect(() => {
         getTiposDocumentos();
     }, []);
-    //AQUI PARA LLENAR LA BUSQEUDA 
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setBusqueda(e.target.value);
         filtrar(e.target.value);
-        console.log("Busqueda", + e.target.value)
-    }
-    const filtrar=(terminoBusqueda: string)=>{
-        var resultadosBusqueda=tablaEncargados.filter((elemento)=>{
-            if(elemento.Nombre.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())||
-            elemento.Apellido.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())){
+        console.log("Busqueda", + e.target.value);
+    };
+
+    const filtrar = (terminoBusqueda: string) => {
+        const resultadosBusqueda = tablaEncargados.filter((elemento) => {
+            if (elemento.Nombre.toLowerCase().includes(terminoBusqueda.toLowerCase()) ||
+                elemento.Apellido.toLowerCase().includes(terminoBusqueda.toLowerCase())) {
                 return elemento;
             }
+            return null;
         });
         setEncargados(resultadosBusqueda);
-    }
-
+    };
 
     const tableHead = ["Id", "Nombre", "Apellido", "Cargo", "Telefono", "Tel. Emergencia", "Correo", "Tip. Documento", "#", "Administrador", "Fecha Registro"];
 
-    // Transformar los datos para las filas de la tabla
-    const tableRows = tablaEncargados.map(encargado => [
+    const tableRows = encargados.map(encargado => [
         encargado.Id,
         encargado.Nombre,
         encargado.Apellido,
-        encargado.Role, // Asumiendo que 'Cargo' se refiere a 'Role'
+        encargado.Role,
         encargado.Telefono,
         encargado.TelEmergencia,
         encargado.Correo,
         encargado.TipoDocumento,
-        encargado.NumDocumento, // Asumiendo que '#' se refiere a 'NumDocumento'
+        encargado.NumDocumento,
         encargado.Administrador,
-        new Date(encargado.FechaRegistro).toLocaleDateString() // Formatear la fecha
+        new Date(encargado.FechaRegistro).toLocaleDateString()
     ]);
 
     return (
@@ -77,16 +78,20 @@ const EncargadoPage = () => {
                     <div className="relative">
                         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                             <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                <path 
+                                    stroke="currentColor" 
+                                    strokeLinecap="round" 
+                                    strokeLinejoin="round" 
+                                    strokeWidth="2" 
+                                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" 
+                                />
                             </svg>
                         </div>
                         <input type="search" id="Buscar" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Buscar por nombre o apelllido" required value={busqueda} onChange={handleChange} />
 
-                        {/* boton de busqyeda */}
                         <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buscar</button>
                     </div>
                 </form>
-
             </div>
             <Table tableHead={tableHead} tableRows={tableRows} />
         </div>

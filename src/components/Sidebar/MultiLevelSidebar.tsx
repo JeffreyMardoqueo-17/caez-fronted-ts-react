cfbcca0ca99469d94844de642c2974afb55daba5
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-09-29 15:19:39 -0600

    ✔🤑 todo listo con el sidebar y el login

--- src/components/Sidebar/MultiLevelSidebar.tsx
+++ src/components/Sidebar/MultiLevelSidebar.tsx
@@ -32,19 +32,42 @@ const menuItems: MenuItem[] = [
         icon: <UserCircleIcon className="h-6 w-6" />,
         submenu: [
             { title: 'List', path: '/Alumno', icon: <ChevronDownIcon className="h-4 w-4" /> },
-            { title: 'Create', path: '/Alumno/create', icon: <ChevronDownIcon className="h-4 w-4" /> },
+            { title: 'Create', path: '/alumno/create', icon: <ChevronDownIcon className="h-4 w-4" /> },
         ],
     },
     {
-        title: 'Settings',
-        icon: <Cog6ToothIcon className="h-6 w-6" />,
+        title: 'Encargados',
+        icon: <UserCircleIcon className="h-6 w-6" />,
+        path: '/Encargados',
+    },
+    {
+        title: 'Tipo de Documento',
+        icon: <InboxIcon className="h-6 w-6" />,
+        path: '/TipoDocumento',
+    },
+    {
+        title: 'Padrinos',
+        icon: <UserCircleIcon className="h-6 w-6" />,
         submenu: [
-            { title: 'Profile', path: '/settings/profile', icon: <ChevronDownIcon className="h-4 w-4" /> },
-            { title: 'Billing', path: '/settings/billing', icon: <ChevronDownIcon className="h-4 w-4" /> },
+            { title: 'List', path: '/padrino', icon: <ChevronDownIcon className="h-4 w-4" /> },
+            { title: 'Create', path: '/padrino/create', icon: <ChevronDownIcon className="h-4 w-4" /> },
         ],
     },
-    { title: 'Reports', icon: <InboxIcon className="h-6 w-6" />, path: '/reports' },
-    { title: 'Encargados', icon: <UserCircleIcon className="h-6 w-6" />, path: '/encargados' },
+    {
+        title: 'Enfermedad',
+        icon: <UserCircleIcon className="h-6 w-6" />,
+        path: '/enfermedad',
+    },
+    {
+        title: 'Confirmación',
+        icon: <InboxIcon className="h-6 w-6" />,
+        path: '/confirmar',
+    },
+    {
+        title: 'Loader',
+        icon: <Cog6ToothIcon className="h-6 w-6" />,
+        path: '/Loader',
+    }
 ];
 
 export const MultiLevelSidebar: React.FC<MultiLevelSidebarProps> = ({ isSidebarOpen, toggleSidebar, toggleTheme, theme }) => {
@@ -57,8 +80,7 @@ export const MultiLevelSidebar: React.FC<MultiLevelSidebarProps> = ({ isSidebarO
 
     return (
         <div
-            className={`transition-all duration-300 ease-in-out bg-white dark:bg-darkTheme-formulario text-black dark:text-white fixed top-0 left-0 h-screen z-40  ${isSidebarOpen ? 'w-64' : 'w-20'
-                }`}
+            className={`transition-all duration-300 ease-in-out bg-white dark:bg-darkTheme-formulario text-black dark:text-white fixed top-0 left-0 h-screen z-40 ${isSidebarOpen ? 'w-64' : 'w-20'}`}
         >
             <div className="flex flex-col h-full">
                 {/* Logo en la parte superior */}
@@ -78,38 +100,64 @@ export const MultiLevelSidebar: React.FC<MultiLevelSidebarProps> = ({ isSidebarO
 
                         return (
                             <li key={index} className="group">
-                                <div
-                                    onClick={() => handleOpen(index)}
-                                    className={`flex items-center p-3 rounded-md cursor-pointer transition-colors duration-300 group-hover:bg-gray-100 dark:group-hover:bg-gray-800 
-                                        ${isActive || hasActiveSubmenu ? 'bg-gray-200 dark:bg-gray-700 text-purple-500' : 'text-gray-600 dark:text-gray-400'}`}
-                                >
-                                    <span className="flex justify-center w-12">
-                                        {item.icon}
-                                    </span>
-                                    {isSidebarOpen && (
-                                        <span className="ml-3 group-hover:text-gray-800 dark:group-hover:text-gray-200">
-                                            {item.title}
+                                {/* Si el item tiene una ruta (no es un submenú), usa NavLink directamente */}
+                                {item.path && !item.submenu && (
+                                    <NavLink
+                                        to={item.path}
+                                        className={({ isActive }) =>
+                                            `flex items-center p-3 rounded-md cursor-pointer transition-colors duration-300 group-hover:bg-gray-100 dark:group-hover:bg-gray-800 
+                                            ${isActive ? 'bg-gray-200 dark:bg-gray-700 text-purple-500' : 'text-gray-600 dark:text-gray-400'}`
+                                        }
+                                    >
+                                        <span className="flex justify-center w-12">
+                                            {item.icon}
                                         </span>
-                                    )}
-                                    {item.submenu && isSidebarOpen && (
-                                        <ChevronDownIcon className={`h-5 w-5 ml-auto transition-transform ${open === index ? 'rotate-180' : ''}`} />
-                                    )}
-                                </div>
+                                        {isSidebarOpen && (
+                                            <span className="ml-3 group-hover:text-gray-800 dark:group-hover:text-gray-200">
+                                                {item.title}
+                                            </span>
+                                        )}
+                                    </NavLink>
+                                )}
 
-                                {item.submenu && open === index && isSidebarOpen && (
-                                    <ul className="pl-6 space-y-2">
-                                        {item.submenu.map((subItem, subIndex) => (
-                                            <li key={subIndex} className="flex items-center p-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-md">
-                                                <NavLink
-                                                    to={subItem.path ?? ''}
-                                                    className={({ isActive }) =>
-                                                        `w-full block p-2 rounded-md transition-colors duration-300 ${isActive ? 'bg-gray-200 dark:bg-gray-700 text-purple-500' : 'text-gray-600 dark:text-gray-400'}`}
-                                                >
-                                                    {subItem.title}
-                                                </NavLink>
-                                            </li>
-                                        ))}
-                                    </ul>
+                                {/* Si el item tiene un submenú */}
+                                {item.submenu && (
+                                    <>
+                                        <div
+                                            onClick={() => handleOpen(index)}
+                                            className={`flex items-center p-3 rounded-md cursor-pointer transition-colors duration-300 group-hover:bg-gray-100 dark:group-hover:bg-gray-800 
+                                                ${hasActiveSubmenu ? 'bg-gray-200 dark:bg-gray-700 text-purple-500' : 'text-gray-600 dark:text-gray-400'}`}
+                                        >
+                                            <span className="flex justify-center w-12">
+                                                {item.icon}
+                                            </span>
+                                            {isSidebarOpen && (
+                                                <span className="ml-3 group-hover:text-gray-800 dark:group-hover:text-gray-200">
+                                                    {item.title}
+                                                </span>
+                                            )}
+                                            {item.submenu && isSidebarOpen && (
+                                                <ChevronDownIcon className={`h-5 w-5 ml-auto transition-transform ${open === index ? 'rotate-180' : ''}`} />
+                                            )}
+                                        </div>
+
+                                        {item.submenu && open === index && isSidebarOpen && (
+                                            <ul className="pl-6 space-y-2">
+                                                {item.submenu.map((subItem, subIndex) => (
+                                                    <li key={subIndex} className="flex items-center p-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-md">
+                                                        <NavLink
+                                                            to={subItem.path ?? ''}
+                                                            className={({ isActive }) =>
+                                                                `w-full block p-2 rounded-md transition-colors duration-300 ${isActive ? 'bg-gray-200 dark:bg-gray-700 text-purple-500' : 'text-gray-600 dark:text-gray-400'}`
+                                                            }
+                                                        >
+                                                            {subItem.title}
+                                                        </NavLink>
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        )}
+                                    </>
                                 )}
                             </li>
                         );
